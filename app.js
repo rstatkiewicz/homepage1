f8cb6a751e2d6b9afac715e1f6ba2ff441e91654
Radoslaw Statkiewicz <radoslawstatkiewicz2@gmail.com>
2019-08-01 14:27:55 -0400

    feat: grid

---
+++ app.js
@@ -0,0 +1,3 @@
+button.addEventListener('click', (e) => {
+            const header = document.querySelector('.header__title--js');
+            header.innerHTML = 'klik, klik';
---
+++ main.js
@@ -0,0 +1,12 @@
+alert('siema');
+const name = 'Radek';
+const age = 32;
+console.log(name);
+console.log(age);
+console.log(`Mam na imie ${name} i mam ${age} lat`);
+console.log('witaj przybyszu');
+const heading = document.querySelector('.heading');
+console.log(heading);
+button.addEventListener('click', (e) => {
+            const header = document.querySelector('.header__title--js');
+            header.innerHTML = 'klik, klik';
